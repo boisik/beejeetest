c73e5b8d4fe3de746ee1e2240859b1eb81545148
boisik <boisik@mail.ru>
2020-06-10 19:25:32 +0300

    Default Changelist

--- index.php
+++ index.php
@@ -5,3 +5,21 @@
  * Date: 10.06.2020
  * Time: 18:43
  */
+
+ini_set('display_errors', 1);
+include('application/core/autoloader.php');
+//require('application/bootstrap.php');
+use Application\Bootstrap;
+class Setup extends Application\Bootstrap
+{
+    /**
+     * Инициализирует настройки проекта
+     */
+    public static function init()
+    {
+
+        parent::init();
+    }
+}
+
+\Setup::init();
