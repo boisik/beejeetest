362ef85cf6bd3c59a553149b6b39111a407bdcd1
boisik <boisik@mail.ru>
2020-06-10 20:04:34 +0300

    инициализация приложения, подключение бд, лоадера итд.

---
+++ application/views/template_view.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 10.06.2020
+ * Time: 19:58
+ */
+?>
+
+<div class="box">
+    <?php include 'application/views/'.$content_view; ?>
+</div>
