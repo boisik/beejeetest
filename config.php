524556dcc0641a0b284a8fd3e98fd24c8a5e9178
boisik <boisik@mail.ru>
2020-06-10 19:24:09 +0300

    Default Changelist

--- config.php
+++ config.php
@@ -5,3 +5,14 @@
  * Date: 10.06.2020
  * Time: 18:42
  */
+
+return array (
+
+    'DB_HOST' => 'localhost',
+    'DB_PORT' => NULL,
+    'DB_SOCKET' => NULL,
+    'DB_NAME' => 'test',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+
+);
