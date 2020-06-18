5cdff8bcb167b63ea3c93034b4212584af9a16b9
boisik <boisik@mail.ru>
2020-06-18 15:57:21 +0300

    еще фиксы на всякий случай

--- test.sql
+++ test.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 15 2020 г., 10:10
--- Версия сервера: 10.3.22-MariaDB
--- Версия PHP: 7.3.17
+-- Время создания: Июн 18 2020 г., 15:31
+-- Версия сервера: 5.6.38
+-- Версия PHP: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,8 +33,8 @@ CREATE TABLE `tasks` (
   `username` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
   `useremail` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` tinyint(1) DEFAULT 0,
-  `edited` tinyint(1) NOT NULL DEFAULT 0
+  `status` tinyint(1) DEFAULT '0',
+  `edited` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -41,11 +42,11 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `username`, `useremail`, `text`, `status`, `edited`) VALUES
-(1, 'fd', 'dfw@fdvs.ti', 'ffffffffffff', 0, 0),
-(2, 'fd', 'dfw@fdvs.ti', 'ffffffffffffkihjkkkkkkkkkkkkkkkkkkkkkkjkjkjkjkjkjkjkj', 0, 0),
-(3, 'fdfewf', 'dfw@fdvs.ti', 'ffffffffffffkihjkkkkkkkkkkkkkkkkkkkkkkjkjkjkjkjkjkjkj', 0, 0),
-(4, 'fdfewf', 'dfw@fdvs.ti', 'ffffffffffffkihjkkkkkkkkkefe', 0, 0),
-(5, 'fdfsdf', 'dffffw@fdvs.ti', 'dfvvvvvvvedf', 0, 0);
+(6, 'AAAALex', 'bbbb@mail.ru', 'custom1111 1', 0, 1),
+(7, 'Boris', 'zbzzanuda@test.com', '            customtextnew    ', 0, 1),
+(8, 'Viktor', 'superuser@super.loca', '         custom1111  ', 0, 0),
+(9, 'Harry', 'bbb@super.local', '         custom1111  ', 0, 0),
+(10, 'Aida', 'aidka@sobaka.com.ua', '      qazxsww!!!@@#  ', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -55,37 +56,19 @@ INSERT INTO `tasks` (`id`, `username`, `useremail`, `text`, `status`, `edited`) 
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hash` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `ip` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `name` varchar(9) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `hash` varchar(20) DEFAULT NULL,
+  `ip` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `password`, `hash`, `ip`) VALUES
-(1, 'yashka', '12345', 'dZamjNY', '127.0.0.1'),
-(2, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', 'dZamjNY', '127.0.0.1'),
-(3, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(4, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(5, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(6, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(7, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(8, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(9, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(10, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(11, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(12, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(13, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(14, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(15, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(16, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(17, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(18, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(19, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL),
-(20, 'yashka1', 'c64be1bdd5440a33281f7d147dd0e6e5', NULL, NULL);
+(1, 'yashka', 'c64be1bdd5440a33281f7d147dd0e6e5', '7YSj9r1', '127.0.0.1'),
+(2, 'admin', '49dc1dadcd922af42d22b5399b5566b8', '7YSj9r1', '127.0.0.1');
 
 --
 -- Индексы сохранённых таблиц
@@ -111,13 +94,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
